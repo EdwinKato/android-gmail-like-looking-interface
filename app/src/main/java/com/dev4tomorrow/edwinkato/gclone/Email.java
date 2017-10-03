78ebca533ecb477808961a22e09018e93451ca5b
Edwin Kato <edwin.kato@andela.com>
2017-10-03 11:55:20 +0300

    Add email model

---
+++ app/src/main/java/com/dev4tomorrow/edwinkato/gclone/Email.java
@@ -0,0 +1,17 @@
+package com.dev4tomorrow.edwinkato.gclone;
+
+/**
+ * Created by edwinkato on 10/3/17.
+ */
+
+public class Email {
+    String title;
+    String description;
+    String date;
+
+    public Email(String title, String description, String date) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+}
